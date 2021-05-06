cf5bb93920a8d51f40c657ea36db5a670e0f619d
gusailov <gusailov@gmail.com>
2021-05-06 19:06:54 +0300

    video repeat

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 class App
   def call(_env)
+    perform_request
     [status, headers, body]
   end
 
   private
+
+  def perform_request
+    sleep rand(2..3)
+  end
 
   def status
     200
@@ -14,6 +19,6 @@ class App
   end
 
   def body
-    []
+    ["Welocome aboard!\n"]
   end
 end
