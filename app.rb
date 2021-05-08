4370f52b4fcc06b1e44da306f7cdcfee72b06e69
gusailov <gusailov@gmail.com>
2021-05-08 13:50:36 +0300

    final fix

--- app.rb
+++ app.rb
@@ -1,29 +1,37 @@
 class App
   def call(env)
-    @req = Rack::Request.new(env)
-    @date_time_format = DateTimeFormat.new(@req.params['format'])
-    perform_request
+    req = Rack::Request.new(env)
 
-    if !path_check?
-      response(404, '')
-    elsif @date_time_format.invalid.any?
-      response(400, "Unknown data time format #{@date_time_format.invalid}\n")
+    if req.path == '/time'
+      time_response(req)
     else
-      response(200, "#{@date_time_format.date_time}\n")
+      response(404, 'The requested resource is not available')
     end
   end
 
   private
 
-  def path_check?
-    @req.path == '/time'
+  def time_response(req)
+    format_string = req.params['format']
+
+    if format_string
+      check_format_response(format_string)
+    else
+      response(400, 'Format not found')
+    end
+  end
+
+  def check_format_response(format_string)
+    date_time_format = DateTimeFormat.new(format_string)
+
+    if date_time_format.invalid.any?
+      response(400, "Unknown data time format #{date_time_format.invalid}\n")
+    else
+      response(200, "#{date_time_format.date_time}\n")
+    end
   end
 
   def response(status, body)
     [status, { 'Content-Type' => 'text/plain' }, [body]]
-  end
-
-  def perform_request
-    sleep rand(2..3)
   end
 end
